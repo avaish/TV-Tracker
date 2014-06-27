20a3b62b2476215fe73e38678d3d1c8d65586b50
Atharv Vaish <atharv.vaish@gmail.com>
2014-06-27 14:20:50 -0400

    Initial Commit

---
+++ TV Tracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TV Tracker
+//
+//  Created by Atharv Vaish on 6/27/14.
+//  Copyright (c) 2014 Atharv Vaish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
